7ee479ba0b82abdb87c4be6a933378a3fe21d26f
X3RMINATION <nicolaeungur@gmail.com>
2022-11-21 20:20:13 -0800

    Results done!!! time for data entry babeee

--- src/main/LCGTest.java
+++ src/main/LCGTest.java
@@ -3,54 +3,44 @@ package main;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class LCGTest {
 
     public static final List<ParameterSet> paramList = new ArrayList<>();
-    public static final int RANDOM_COUNT = 100_000_000;
+    public static final int TIME_COUNT = 100_000_000;
+    public static final int INTERVAL_COUNT = 100_000;
 
     public static int seed;
 
     public static void main(String[] args) throws IOException {
         seed = new Random().nextInt();
+        if(seed < 0) seed *= -1;
         System.out.println("Seed is " + seed);
         paramList.add(new ParameterSet("C++", 22695477, 1, (long) (Math.pow(2, 32))));
-//        paramList.add(new ParameterSet("glibc", 1103515245, 1, (long) (Math.pow(2, 31))));
-//        paramList.add(new ParameterSet("TurboPascal", 134775813, 1, (long) (Math.pow(2, 32))));
-//        paramList.add(new ParameterSet("MicrosoftVisualC++", 214013, 2531011, (long) (Math.pow(2, 32))));
-//        paramList.add(new ParameterSet("MicrosoftVisualBasic", 1140671485, 12820163, (long) (Math.pow(2, 34))));
-//        paramList.add(new ParameterSet("NativeAPI", 2147483629, 2147483587, (long) (Math.pow(2, 31) - 1)));
-//        paramList.add(new ParameterSet("C++11", 48271, 0, (long) (Math.pow(2, 31) - 1)));
-//        paramList.add(new ParameterSet("MMIX", 6364136223846793005L, 1442695040888963407L, (long) (Math.pow(2, 64))));
-//        paramList.add(new ParameterSet("Newlib", 6364136223846793005L, 1, (long) (Math.pow(2, 64))));
-//        paramList.add(new ParameterSet("Java", 25214903917L, 11, (long) (Math.pow(2, 48))));
-//        paramList.add(new ParameterSet("random0", 8121, 28411, (long) (Math.pow(2, 3)*Math.pow(7, 5))));
-//        paramList.add(new ParameterSet("cc65", 65793, 826366247, (long) (Math.pow(2, 23))));
-//        paramList.add(new ParameterSet("RANDU", 65539, 0, (long) (Math.pow(2, 31))));
-        for(ParameterSet param : paramList) {
-            printTimeResult(param);
-            printPeriodResult(param);
-            spectralTest(param);
+        paramList.add(new ParameterSet("glibc", 1103515245, 1, (long) (Math.pow(2, 31))));
+        paramList.add(new ParameterSet("TurboPascal", 134775813, 1, (long) (Math.pow(2, 32))));
+        paramList.add(new ParameterSet("MicrosoftVisualC++", 214013, 2531011, (long) (Math.pow(2, 32))));
+        paramList.add(new ParameterSet("MicrosoftVisualBasic", 1140671485, 12820163, (long) (Math.pow(2, 34))));
+        paramList.add(new ParameterSet("NativeAPI", 2147483629, 2147483587, (long) (Math.pow(2, 31) - 1)));
+        paramList.add(new ParameterSet("C++11", 48271, 0, (long) (Math.pow(2, 31) - 1)));
+        paramList.add(new ParameterSet("Java", 25214903917L, 11, (long) (Math.pow(2, 48))));
+        paramList.add(new ParameterSet("random0", 8121, 28411, (long) (Math.pow(2, 3)*Math.pow(7, 5))));
+        paramList.add(new ParameterSet("cc65", 65793, 826366247, (long) (Math.pow(2, 23))));
+        paramList.add(new ParameterSet("RANDU", 65539, 0, (long) (Math.pow(2, 31))));
+        for(ParameterSet set : paramList) {
+            spectralTest(set);
+            toFile(set);
         }
     }
 
-    private static void printTimeResult(ParameterSet set) {
-        System.out.println("Average time is: " + timeTest(set) + " milliseconds to generate " + RANDOM_COUNT + " numbers");
-    }
-
-    private static void printPeriodResult(ParameterSet set) throws IOException {
-        System.out.println("It takes " + set.modulus() + " iterations for these parameters to repeat given seed " + seed);
+    private static long periodTest(ParameterSet set) throws IOException {
+        return set.modulus();
     }
     public static long timeTest(ParameterSet set) {
         LinearCongruentialGenerator lcg = new LinearCongruentialGenerator(seed, set.multiplier(), set.increment(), set.modulus());
         long time = System.currentTimeMillis();
-        for (int j = 0; j < RANDOM_COUNT; j++) {
+        for (int j = 0; j < TIME_COUNT; j++) {
             lcg.next();
         }
         return System.currentTimeMillis() - time;
@@ -58,20 +48,73 @@ public class LCGTest {
 
     private static void spectralTest(ParameterSet set) throws IOException {
         LinearCongruentialGenerator lcg = new LinearCongruentialGenerator(seed, set.multiplier(), set.increment(), set.modulus());
-        File file = new File("D:\\Documents\\EE test\\src\\results\\" + set.name().toLowerCase() + ".txt");
+        File file = new File("D:\\Documents\\EE test\\src\\lcg_results\\" + set.name().toLowerCase() + ".txt");
         file.createNewFile();
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.append("x, y, z\n");
-        long a = lcg.getCurrent();
-        long b = lcg.next();
-        long c = lcg.next();
-        for(int i = 0; i < 100_000; i++) {
-            fileWriter.append(a + ", " + b + ", " + c + "\n");
-            a = b;
-            b = c;
-            c = lcg.next();
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.append("x, y, z\n");
+            long a = lcg.getCurrent();
+            long b = lcg.next();
+            long c = lcg.next();
+            for (int i = 0; i < 100_000; i++) {
+                fileWriter.append(a + ", " + b + ", " + c + "\n");
+                a = b;
+                b = c;
+                c = lcg.next();
+            }
         }
-        fileWriter.close();
     }
 
+    private static int[] intervalTest(ParameterSet set, int intervals) {
+        LinearCongruentialGenerator lcg = new LinearCongruentialGenerator(seed, set.multiplier(), set.increment(), set.modulus());
+
+        //Because some LCGS are special :)
+        //Ugly solution
+        int modifier = 0;
+        double power = Math.log(set.modulus()) / Math.log(2);
+        if(power != (int) power) {
+            modifier = 1;
+        }
+        int[] distribution = new int[intervals];
+        for (int i = 0; i < INTERVAL_COUNT; i++) {
+            float next = lcg.nextFloat(modifier);
+            distribution[(int)(next*10)]++;
+        }
+        return distribution;
+    }
+
+    private static double chiSquareTest(double[] distribution, int intervals) {
+        double chi = 0;
+        //Null hypothesis, probably doesn't need to be float
+        float n = INTERVAL_COUNT / (float) intervals;
+        for(int i = 0; i < intervals; i++) {
+            chi += Math.pow(distribution[i] - n, 2) / n;
+        }
+        return chi;
+    }
+
+    //Spectral test results are seperate
+    private static void toFile(ParameterSet set) throws IOException {
+        System.out.println(set.name());
+        File file = new File("D:\\Documents\\EE test\\src\\results\\" + set.name().toLowerCase() + ".txt");
+        file.createNewFile();
+        double averageTime = 0;
+        double[] averageIntervals = new double[10];
+        for (int i = 0; i < 20; i++) {
+            seed = new Random().nextInt();
+            averageTime += timeTest(set);
+            for(int j = 0; j < averageIntervals.length; j++) {
+                averageIntervals[j] += intervalTest(set, 10)[j];
+            }
+        }
+        averageTime/=20;
+        for(int i = 0; i < averageIntervals.length; i++) {
+            averageIntervals[i] /= 20;
+        }
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.append("Time for " + TIME_COUNT + " numbers is ").append(String.valueOf(averageTime)).append(" milliseconds \n");
+            fileWriter.append("Repeats after ").append(String.valueOf(periodTest(set))).append(" numbers of output\n");
+            fileWriter.append("Interval of float outputs given 10 intervals: ").append(Arrays.toString(averageIntervals)).append("\n");
+            fileWriter.append("Chi square value of float outputs given 10 intervals: ").append(String.valueOf(chiSquareTest(averageIntervals, 10))).append("\n");
+        }
+    }
 }
--- src/main/LinearCongruentialGenerator.java
+++ src/main/LinearCongruentialGenerator.java
@@ -19,7 +19,24 @@ public class LinearCongruentialGenerator {
         return getCurrent();
     }
 
+    public boolean nextBoolean() {
+        System.out.println(getBits());
+        return next() >> (getBits() - 1) != 0;
+    }
+
+    public float nextFloat(int modifier) {
+        int bits = (getBits() - 1) /2;
+        float next = (next() >> bits) / ((float)(1 << (bits + modifier)));
+        if(next < 0) next *= -1;
+        //random0 doesn't like to behave
+        return Math.min(next, 0.999999F);
+    }
+
     public long getCurrent() {
         return current;
+    }
+
+    private int getBits() {
+        return (int)(Math.log(this.modulus) / Math.log(2) + 1);
     }
 }
